c87f27508987cc7b5f13de169f993507aa80d87f
Tom Holmes <tholmes59@gmail.com>
2019-01-16 16:57:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,14 @@
 def square_array(array)
-  new_array = []
+  array.collect do |number|
+  number**2 
+end
+end
+
+
+
+
+new_array = []
   array.each do |number|
   new_array.push number**2
   end
   new_array
-end
-
-array.collect do number
